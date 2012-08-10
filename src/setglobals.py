2002910f36f037ad837d7cef7ec5fa0369f1a19c
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2012-08-10 16:07:18 -0400

    internal refactoring of setglobals
    this version has a far more robust implementation of the calling_frame
    function.

--- src/setglobals.py
+++ src/setglobals.py
@@ -6,16 +6,11 @@ _params= dict(
     SGDEBUG__ = False
 )
 
-def calling_module(level=1, _mods=sys.modules, _main=sys.modules['__main__']):
-    st = ins.stack()
-    # frame, filename, line_num, func, source_code, source_index = st[level]
-    filename = st[level][1]
-    modulename = ins.getmodulename(filename)
-    try:
-        return _mods[modulename]
-    except KeyError, e:
-        assert _main.__file__ == filename
-        return _main
+def calling_frame(level=0):
+    return ins.stack()[level+2][0]
+
+def calling_module(level=0):
+    return ins.getmodule(calling_frame(1))
 
 def _setglobals(defaults, globs, fromenv,
                 _environment=dict(os.environ)):
@@ -27,8 +22,8 @@ def _setglobals(defaults, globs, fromenv,
         if SGDEBUG__:
             print (u'found: %s=%r' % (key, val)).encode('utf8')
 
-def setglobals(defaults, globs=None, level=1):
-    mod = calling_module(level + 1)
+def setglobals(defaults, globs=None, level=0):
+    mod = calling_module()
     _setglobals(defaults,
                 vars(mod) if globs is None else globs,
                 mod.__name__ == '__main__')
