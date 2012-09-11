9660c4b893a4820775224dcf932b9bc9b8e8d559
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2012-09-11 16:48:33 -0400

    added spread(seq, N, K) generator...
    ...plus extended other functions (e.g. partition) to strings.

--- src/partition.py
+++ src/partition.py
@@ -11,8 +11,7 @@ def partition(seq, pred):
     >>> pred = lambda x: int(x) % 3 == 2
     >>> seq = map(str, range(15))
     >>> partition(seq, pred)
-    [('2', '5', '8', '11', '14'),
-     ('0', '1', '3', '4', '6', '7', '9', '10', '12', '13')]
+    [('2', '5', '8', '11', '14'), ('0', '1', '3', '4', '6', '7', '9', '10', '12', '13')]
     """
 
     if not tc.issequence(seq):
@@ -29,25 +28,20 @@ def ipartition(seq, pred):
     """
     >>> from partition import ipartition
     >>> pred = lambda x: int(x) % 3 == 2
-    >>> seq = imap(str, xrange(15))
-    >>> ipartition(seq, pred)
-    [<itertools.ifilter at 0x33193d0>, <itertools.ifilterfalse at 0x3319a10>]
+    >>> seq = it.imap(str, xrange(15))
+    >>> map(type, ipartition(seq, pred))
+    [<type 'itertools.ifilter'>, <type 'itertools.ifilterfalse'>]
     >>> map(tuple, ipartition(seq, pred))
-    [('2', '5', '8', '11', '14'),
-     ('0', '1', '3', '4', '6', '7', '9', '10', '12', '13')]
+    [('2', '5', '8', '11', '14'), ('0', '1', '3', '4', '6', '7', '9', '10', '12', '13')]
     """
-    if not tc.issequence(seq):
-        raise TypeError('seq is not a sequence')
 
     t1, t2 = it.tee(seq)
     return [it.ifilter(pred, t1), it.ifilterfalse(pred, t2)]
 
 
 def kgroups(seq, k):
-    """ Yield successive k-sized groups from l.
+    """Yield successive k-sized groups from l.
     """
-    if not tc.issequence(seq):
-        raise TypeError('seq is not a sequence')
     if not k > 0:
         raise ValueError('k must be positive')
 
@@ -56,7 +50,7 @@ def kgroups(seq, k):
 
 
 def splitseq(seq, m, truncate=False):
-    """ Yield (approximately) m subsequences from seq.
+    """Yield (approximately) m subsequences from seq.
 
     If n = len(seq), and n >= m, then the function will yield m
     consecutive subsequences of length n//m.  Then, if r = n % m > 0
@@ -65,9 +59,6 @@ def splitseq(seq, m, truncate=False):
 
     If n < m, a ValueError is raised.
     """
-
-    if not tc.issequence(seq):
-        raise TypeError('seq is not a sequence')
 
     n = len(seq)
     if not isinstance(m, int) or n < m:
@@ -80,3 +71,87 @@ def splitseq(seq, m, truncate=False):
         yield seq[i:j]
         i = j
 
+__DEFAULT=object()
+
+def spread(seq, N=__DEFAULT, K=1):
+    """Yield (at most N) successive seq subsequences of length at least K.
+
+    seq    any object supporting len(...) and slice-indexing
+    N      a positive integer (default: L = len(seq))
+    K      a positive integer not greater than L (default: 1)
+
+    The subsequences yielded represent a "tiling" of seq.  This means that they
+    are adjacent in seq, and cover all of seq.  In other words, each element of
+    seq belongs to exactly one of the yielded subsequences.
+
+    With these conventions, the specification above can be sharpened to:
+
+    Yield n = min(N, L//K) successive subsequences of seq, the first L % n
+    having length k + 1 (where k = L//n), and the remaining ones having
+    length k.
+
+    >>> tuple(spread('abcdefghij', N=4))
+    ('abc', 'def', 'gh', 'ij')
+    >>> tuple(spread('abcdefghij', K=3))
+    ('abcd', 'efg', 'hij')
+    >>> tuple(spread('abcdefghij', N=4, K=3))
+    ('abcd', 'efg', 'hij')
+    >>> tuple(spread('abcdefghijklmnopqrstuvwxyz', N=4))
+    ('abcdefg', 'hijklmn', 'opqrst', 'uvwxyz')
+
+    Note that n < N and k > K can simultaneously hold:
+    >>> tuple(spread('abcdefghijklmnopqrstuvwxyz', N=4, K=7))
+    ('abcdefghi', 'jklmnopqr', 'stuvwxyz')
+
+    >>> tuple(spread('x', N=None))
+    Traceback (most recent call last):
+    TypeError: N must be a positive integer
+    >>> tuple(spread('x', N=0))
+    Traceback (most recent call last):
+    TypeError: N must be a positive integer
+    >>> tuple(spread('x', K=None))
+    Traceback (most recent call last):
+    TypeError: K must be a positive integer
+    >>> tuple(spread('x', K=0))
+    Traceback (most recent call last):
+    TypeError: K must be a positive integer
+    >>> tuple(spread('x', K=2))
+    Traceback (most recent call last):
+    TypeError: K must not exceed len(seq)
+    """
+
+    L = len(seq)
+    if N is __DEFAULT: N = L
+    if not (type(N) == int and 0 < N):
+        raise TypeError('N must be a positive integer')
+    if not (type(K) == int and 0 < K):
+        raise TypeError('K must be a positive integer')
+    elif K > L:
+        raise TypeError('K must not exceed len(seq)')
+
+    n = min(L//K, N)
+    k, rem = divmod(L, n)
+    # the generator will yield a total of n <= N subsequences, the
+    # first rem of which having length k + 1, and the remaining ones
+    # having length k >= K.
+
+    start = 0
+    stride = k + 1
+    for i in range(rem):
+        end = start + stride
+        yield seq[start:end]
+        start = end
+
+    stride = k
+    for i in range(rem, n):
+        end = start + stride
+        yield seq[start:end]
+        start = end
+
+    assert start == L
+
+
+if __name__ == '__main__':
+    # spread('x', N=None)
+    import doctest
+    doctest.testmod()
