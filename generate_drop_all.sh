8ca7433e04b4bbd8f6a17b31b9f69af9c0a80bb4
Sean D Erickson <seanderickson@github.com>
2012-09-27 13:04:41 -0400

    script change

--- generate_drop_all.sh
+++ generate_drop_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]
+if [[ $# -lt 2 ]]
 then
   echo "generate_drop_all.sh <user> <db>"
   exit 1;
@@ -8,7 +8,8 @@ fi
 
 USER=$1
 DB=$2
+HOST=$3
 echo "begin;" 
-psql -U$USER $DB -c \\d |grep table |grep example |nawk '{print "drop table " $3 " cascade;" }'
-psql -U$USER $DB -c \\d |grep sequence |grep example |nawk '{print "drop sequence " $3 " cascade;" }'
+psql -U$USER $DB -h $HOST -c \\d |grep table |grep example |nawk '{print "drop table " $3 " cascade;" }'
+#psql -U$USER $DB -c \\d |grep sequence |grep example |nawk '{print "drop sequence " $3 " cascade;" }'
 echo "commit;"
