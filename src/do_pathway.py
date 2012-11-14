939805dc87d4bf3fd6c80f2e2cd7fcfc7c623266
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-11-14 00:35:33 -0500

    link proteins to the db

--- src/do_pathway.py
+++ src/do_pathway.py
@@ -13,13 +13,13 @@ import signature
 
 # mapping from target names in signature.LATEST to names in the diagram
 target_name_fixups = {
-    u'JNK1': 'JNK1-2-3',
-    u'JNK2': 'JNK1-2-3',
-    u'JNK3': 'JNK1-2-3',
+    u'JNK1': 'JNK',
+    u'JNK2': 'JNK',
+    u'JNK3': 'JNK',
     u'AKT1': 'AKT',
     u'AKT2': 'AKT',
     u'AKT3': 'AKT',
-    u'MNK2': 'MNK1-2',
+    u'MNK2': 'MNK',
     u'CHK': 'CHK1-2',
     u'PI3K-ALPHA': 'PI3K',
     u'PI3K-BETA': 'PI3K',
@@ -33,11 +33,11 @@ target_name_fixups = {
     u'B-RAF': 'RAF',
     u'C-RAF': 'RAF',
     u'P38-ALPHA': 'P38',
-    u'P38-BETA': 'p38',
+    u'P38-BETA': 'P38',
     u'RSK2': 'P90RSK',
-    u'JAK1': 'JAK1-2-3',
-    u'JAK2': 'JAK1-2-3',
-    u'JAK3': 'JAK1-2-3',
+    u'JAK1': 'JAK',
+    u'JAK2': 'JAK',
+    u'JAK3': 'JAK',
     u'PKC-B': 'PKC',
     u'CHK1': 'CHK1-2',
     u'GSK-3': 'GSK3',
@@ -66,9 +66,76 @@ target_name_fixups = {
     u'ALK': 'ALK2',
     u'ALK': 'ALK5',
     u'PLK1': 'PLK',
-    u'PLK2': 'PLK',
+    u'PLK3': 'PLK',
+    u'BCL2': 'BCL-2',
     # didn't map PARP-2 to PARP since compound lists for both are currently identical
     }
+
+target_display_names = {
+    'AURORA': 'Aurora',
+    'BCL-2': 'Bcl-2',
+    'C-ABL': 'c-Abl',
+    'C-KIT': 'c-Kit',
+    'C-MET': 'c-Met',
+    'CDC25': 'Cdc25',
+    'CHK1-2': 'CHK1/2',
+    'ERK1-2': 'ERK1/2',
+    'MEK1-2': 'MEK1/2',
+    'MTOR': 'mTOR',
+    'P38': 'p38',
+    'P53': 'p53',
+    'P90RSK': 'p90RSK',
+    'RAF': 'Raf',
+    'SRC': 'Src',
+    'WEE1': 'Wee1',
+    }
+
+target_protein_ids = {
+    'AKT': (('200482', 'AKT1'), ('200483', 'AKT2'), ('200484', 'AKT3')),
+    'AMPK': (('200486', 'AMPK-alpha1'), ('200487', 'AMPK-alpha2')),
+    'ATM': '200488',
+    'AURORA': (('200489', 'AURKA'), ('200490', 'AURKB'), ('200491', 'AURKC')),
+    'BCL-2': '200493',
+    'C-ABL': (('200495', 'c-Abl'), ('200566', 'Bcr-Abl')),
+    'C-KIT': '200501',
+    'C-MET': '200502',
+    'CDK': (('200498', 'CDK1'), ('200575', 'CDK2'), ('200576', 'CDK4'), ('200577', 'CDK5'), ('200578', 'CDK6'), ('200579', 'CDK7'), ('200580', 'CDK9')),
+    'CHK1-2': (('200499', 'CHK1'), ('200500', 'CHK2')),
+    'CSF1R': '200504',
+    'DDR1': '200505',
+    'DNA-PK': '200506',
+    'EGFR': '200507',
+    'EPHB3': '200508',
+    'ERBB2': '200509',
+    'ERK1-2': (('200510', 'ERK1'), ('200511', 'ERK2')),
+    'ERK5': '200512',
+    'FAK': '200513',
+    'FGFR': (('200514', 'FGFR1'), ('200591', 'FGFR3')),
+    'FLT3': '200515',
+    'GSK3': (('200516', 'GSK3A'), ('200517', 'GSK3B')),
+    'HSP90': (('200594', 'HSP90 alpha'), ('200595', 'HSP90 beta')),
+    'IGF1R': '200519',
+    'IKK': (('200520', 'IKK-alpha'), ('200521', 'IKK-beta'), ('200522', 'IKK-epsilon')),
+    'JAK': (('200523', 'JAK1'), ('200524', 'JAK2'), ('200525', 'JAK3')),
+    'JNK': (('200526', 'JNK1'), ('200527', 'JNK2'), ('200528', 'JNK3')),
+    'MDM2': '200529',
+    'MEK1-2': (('200530', 'MEK1'), ('200531', 'MEK2')),
+    'MEK5': '200532',
+    'MNK': (('200535', 'MNK1'), ('200536', 'MNK2')),
+    'MTOR': '200537',
+    'P38': (('200539', 'p38-alpha'), ('200540', 'p38-beta'), ('200541', 'p38-delta'), ('200542', 'p38-gamma')),
+    'P53': '200543',
+    'P90RSK': '200545',
+    'PARP': (('200546', 'PARP'), ('200609', 'PARP-2')),
+    'PDK1': '200547',
+    'PI3K': (('200634', 'PI3K-alpha'), ('200635', 'PI3K-beta'), ('200636', 'PI3K-delta'), ('200637', 'PI3K-gamma'),),
+    'PKC': '200549',
+    'PLK': (('200551', 'PLK1'), ('200614', 'PLK2'), ('200615', 'PLK3')),
+    'RAF': (('200494', 'B-Raf'), ('200503', 'C-Raf')),
+    'SRC': '200555',
+    'WEE1': '200558',
+    }
+
 
 if __name__ == '__main__':
 
@@ -161,11 +228,18 @@ if __name__ == '__main__':
         # update new signature_data
         signature_data[target] = compounds
         # update context data
-        signatures_ctx.append({
-                'target_name': target,
-                'compounds': compounds,
-                'show_scale': any(c.signature for c in compounds)
-                })
+        ctx = {
+            'target_name': target,
+            'target_display': target_display_names.get(target, target),
+            'compounds': compounds,
+            'show_scale': any(c.signature for c in compounds)
+            }
+        protein_id = target_protein_ids[target]
+        if isinstance(protein_id, basestring):
+            ctx['protein_id_single'] = protein_id
+        else:
+            ctx['protein_id_list'] = protein_id
+        signatures_ctx.append(ctx)
     # build context data for cell lines and calculate midpoint
     cell_lines_ctx = list(enumerate(signature.cell_lines))
     cut_idx = int(math.ceil(len(cell_lines_ctx) / 2.0))
