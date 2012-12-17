4d801daeebccb7944919a350b0f364ecf28b4da2
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2012-12-17 16:14:35 -0500

    fix popup placement
    Previous version did not properly account for scrolling; this version
    also takes into account of all four edges of the viewport (not just
    the right edge), uses more accurate values for the popup's dimensions,
    and has clearer, better-documented code.

--- django/responses/static/responses/js/responses.js
+++ django/responses/static/responses/js/responses.js
@@ -68,6 +68,39 @@ jQuery(document).ready(function ($) {
         scroll: remove_popup,
     });
 
+    function pxtoint(s) {
+        return parseInt(s.replace(/px\s*$/i, ''));
+    }
+
+    function get_popup_margins($popup) {
+        var margins = {t: pxtoint($popup.css('margin-top')),
+                       r: pxtoint($popup.css('margin-right')),
+                       b: pxtoint($popup.css('margin-bottom')),
+                       l: pxtoint($popup.css('margin-left'))};
+                       
+        // function redefines itself as a closure, with memoized return value
+        get_popup_margins = function ($p) { return margins; }
+        return get_popup_margins($popup);
+    }
+
+    function get_popup_lr_offset($popup) {
+        // returns x- and y-offsets of popup's lower-right corner
+        // (relative to popup's position)
+        var margins = get_popup_margins($popup);
+        var slack = 1;
+        var dx = margins.r + slack;
+        var dy = margins.b + slack;
+
+        // function redefines itself as a closure
+        get_popup_lr_offset = function ($p) {
+            return {x: $p.outerWidth() + dx, y: $p.outerHeight() + dy};
+        }
+
+        return get_popup_lr_offset($popup);
+    }
+
+    var CURSORWIDTH = 12;
+
     $('.scatterplot').bind({
         mousedown: function (e) {
             e.stopPropagation();
@@ -85,9 +118,50 @@ jQuery(document).ready(function ($) {
             for (var i = 0; i < rows.length; ++i) inner += rows[i];
             var $popup = $('<div id="popup"><table>' + inner + '</table></div>').
                          appendTo('body');
-            var w = $popup.width();
-            $popup.offset({left: Math.min(e.pageX + 10 + w, $(window).width() - 8) - w,
-                           top: e.pageY});
+
+            // x0 and y0 give the preferred offset for the popup,
+            // barring conflicts with the current viewport's edges
+            var x0 = e.pageX + CURSORWIDTH;
+            var y0 = e.pageY;
+
+            var lr_offset = get_popup_lr_offset($popup);
+            var margins = get_popup_margins($popup);
+
+            var $win = $(window);
+
+            // l, r, t, and b give the x- (l, r) or y-coordinates (t,
+            // b) for of the current viewport's left, right, top, and
+            // bottom edges, respectively
+            var l = $win.scrollLeft();
+            var r = l + $win.width();
+            var t = $win.scrollTop();
+            var b = t + $win.height();
+
+            // xl and xr give the x-offsets that would put the popup
+            // up against the viewport's left and right edges,
+            // respectively
+            var xl = l + margins.l;
+            var xr = r - lr_offset.x;
+
+            // yt and yb give the y-offsets that would put the popup
+            // up against the viewport's top and bottom edges,
+            // respectively
+            var yt = t + margins.t;
+            var yb = b - lr_offset.y;
+
+            // the expression below implements the following policy
+            // (for the horizontal and vertical dimensions considered
+            // separately):
+            // - if the viewport is not wide|tall enough to fully
+            //   contain the popup, position the popup against the
+            //   viewport's left|top edge;
+            // - otherwise, if positioning the popup at x0|y0 would
+            //   cause it to extend beyond the viewport's right|bottom
+            //   edge, then position it against the viewport's
+            //   right|bottom edge instead;
+            // - otherwise, position the popup at x0|y0.
+            $popup.offset({left: Math.max(xl, Math.min(xr, x0)),
+                           top:  Math.max(yt, Math.min(yb, y0))});
         },
     });
 });
