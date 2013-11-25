a0176ea92f2ad5534658728409d6dad08b5582b0
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-11-25 12:01:53 -0500

    new name: scatterplot_browser

--- django/hmslincs_server/urls.py
+++ django/hmslincs_server/urls.py
@@ -43,9 +43,9 @@ urlpatterns = patterns('',
      'django.views.generic.simple.direct_to_template',
      {'template': '10_1038_nchembio_1337__fallahi_sichani_2013/dose_response_grid.html'}),
 
-    (r'^explore/10.1038-nchembio.1337/fallahi-sichani-2013/scatterplots\.html$',
+    (r'^explore/10.1038-nchembio.1337/fallahi-sichani-2013/scatterplot_browser\.html$',
      'django.views.generic.simple.direct_to_template',
-     {'template': '10_1038_nchembio_1337__fallahi_sichani_2013/scatterplots.html'}),
+     {'template': '10_1038_nchembio_1337__fallahi_sichani_2013/scatterplot_browser.html'}),
 )
 
 # For DEBUG mode only (development) serving of static files
