7cb0cb0fac2a197b0462844f802c0f88b1af3d80
Jeremy Muhlich <jmuhlich@bitflood.org>
2014-02-18 13:04:22 -0500

    lazy loading for popup images

--- signaling/explore/.etc/js/main.js
+++ signaling/explore/.etc/js/main.js
@@ -28,9 +28,9 @@ jQuery(document).ready(
                 show: 'blind',
                 width: 'auto',
                 height: 'auto',
-                position:  {my: 'left', at: 'right', of: this}
+                position: {my: 'left top', at: 'right top', of: this},
+                open: function (event) { media_ondemand_load(event.target); }
             });
-            
         });
 
         /*
@@ -115,6 +115,38 @@ jQuery(document).ready(
                               $sb.height() + $sbl.height());
             var offset = $window.height() - sbl_bottom;
             return offset > 10;
+        }
+
+        function media_ondemand_load(target) {
+            /*
+              Implement semi-semantic lazy loading of media elements.
+              =====
+
+              target: DOM node or jQuery selector denoting container within
+                which to perform loading.
+
+              This page has many initially hidden jquery-ui dialogs containing
+              <img> tags, but we want the browser to put off fetching their
+              content until it's actually displayed. The typical img trick of
+              converting it to a css background is undesirable as the image is
+              the actual content.
+
+              Here we use an idiom in which we include the media tag as usual
+              except for stripping off the src attribute. We then put an <a> tag
+              with a class of "media-ondemand" immediately preceeding the media
+              tag, and set its href attribute to the value of the stripped src
+              attribute from the media tag. Here on dialog open we reverse that
+              transformation, leaving only fully functional media elements
+              behind.
+            */
+            $('a.media-ondemand', target).each(function () {
+                var $link = $(this);
+                var $media = $link.next();
+                // Copy the <a>'s href to the media's src.
+                $media.attr('src', $link.attr('href'));
+                // Delete the <a> entirely.
+                $link.remove();
+            });
         }
     }
 );
