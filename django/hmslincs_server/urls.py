c37fa44ad3404f073d091eb40210d725ba1011dc
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-03-10 16:24:24 -0400

    scatterplot browser for basals

--- django/hmslincs_server/urls.py
+++ django/hmslincs_server/urls.py
@@ -73,6 +73,14 @@ urlpatterns = patterns('',
      'django.views.generic.simple.direct_to_template',
      {'template': '10_1038_nchembio_1337__fallahi_sichani_2013/scatterplot_browser_content.html'}),
 
+    (r'^explore/10.1038-nchembio.1337/fallahi-sichani-2013/scatterplot_browser_content\.html$',
+     'django.views.generic.simple.direct_to_template',
+     {'template': '10_1038_nchembio_1337__fallahi_sichani_2013/scatterplot_browser_content.html'}),
+
+    (r'^explore/responses/scatterplot_browser\.html$',
+     'django.views.generic.simple.direct_to_template',
+     {'template': 'responses/scatterplot_browser.html'}),
+
     # breast_cancer_signaling is currently served by apache directly from
     # STATIC_ROOT, so no url patterns are listed here.
 )
